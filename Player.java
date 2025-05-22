d7d7104feaeec28bad4b59af0416b0a1f48c64ca
James Lei <bestpsyduckever@gmail.com>
2025-05-22 09:54:42 -0400

    Implemented parts of Player and Coordinate

--- Player.java
+++ Player.java
@@ -1,3 +1,15 @@
-public class Character {
-
+abstract class Character extends Entity {
+  private String name;
+  private Resource health;
+  public Character(String name, int maxHealth) {
+    super("Character");
+    this.name = name;
+    health = new Resource(maxHealth);
+  }
+  public String getName() {
+    return name;
+  }
+  public String getHealth() {
+    return health.toString();
+  }
 }
--- Resource.java
+++ Resource.java
@@ -1,8 +1,15 @@
 public class Resource {
   private int current, max;
   private String name;
+  public Resource(int max) {
+    this.max = max;
+    current = max;
+  }
   public String toString() {
     return current + " / " + max;
+  }
+  public void restore() {
+    current = max;
   }
   public void restore(int amount) {
     current = (current + amount) % max;
