340433f52e6a7a5c7252e4dac8ffdca2681b8aa0
James Lei <bestpsyduckever@gmail.com>
2025-05-22 09:47:22 -0400

    Implemented Resource.java

---
+++ Resource.java
@@ -0,0 +1,15 @@
+public class Resource {
+  private int current, max;
+  private String name;
+  public String toString() {
+    return current + " / " + max;
+  }
+  public void restore(int amount) {
+    current = (current + amount) % max;
+  }
+  public boolean consume(int amount) {
+    if (amount > current) return false;
+    current -= amount;
+    return true;
+  }
+}
