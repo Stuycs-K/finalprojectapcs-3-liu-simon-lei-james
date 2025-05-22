c032e8eb5dc2720733a2369f44dec09e20cd1285
lesimongus <sliu60@stuy.edu>
2025-05-22 09:14:38 -0500

    started work on game

--- DEVLOG-Amy.md
+++ DEVLOG-Amy.md
@@ -2,10 +2,10 @@
 
 This document must be updated daily every time you finish a work session.
 
-## Amy Smith CHANGE THE NAME FOOL!
+## Simon Liu
 
-### 2024-01-02 - Brief description
-Expanded description including how much time was spent on task.
+### 2025-05-22 - Began work on coding the Game class
+Around half of the class period was spent on writing the Game class. Some of the fields and the functions require the presence of the other classes, namely Tile and Character, so those had to be commented out for the time being.
 
 ### 2024-01-03 - Brief description
 Expanded description including how much time was spent on task.
---
+++ Game.java
@@ -0,0 +1,36 @@
+public class Game{
+
+  private static int turn; //turn is incremented each time phase is switched
+  private static boolean playerPhase, victory, enemyVictory;
+  /*private Tile[][] board; commented out for now until Tile is implemented
+  private ArrayList<Enemy>enemies = new ArrayList<Enemy>();
+  private ArrayList<Player>players = new ArrayList<Player>(); all commented out until respective classes get implemented*/
+
+  public static void setup(){
+    turn = 1;
+    playerPhase = true;
+    victory = false;
+    enemyVictory = false;
+  }
+
+  public static boolean checkVictory(){ //currently the only victory condition is rout, but other potential goals like seizing a point or defending for a specific number of turns may also be added
+    if (enemies.size() <= 0){
+      victory = true;
+    }
+    if (players.size() <= 0){
+      enemyVictory = true;
+    }
+  }
+
+  public static void run(){
+    while (!victory && !enemyVictory){
+      checkVictory();
+
+    }
+  }
+
+  public static void main(String[] args){
+    setup();
+    run();
+  }
+}
